35b650962de63b23373037ead3fa71e6b4780d17
dritoferro <adrianotagliaferro@hotmail.com>
2019-11-15 14:41:39 -0300

    Added bcrypt for password security; Implemented bcrypt.

--- src/service/UsuarioService.ts
+++ src/service/UsuarioService.ts
@@ -3,12 +3,20 @@ import { Usuario } from '../domain/Usuario';
 import { ObjectId } from 'mongodb';
 import { UsuarioUpdate } from '../domain/UsuarioUpdate';
 import { prepareMessage } from './KafkaService';
+import { genSalt, hash, compare } from 'bcrypt';
 
-//TODO implementar encriptação da senha antes de fazer a criação.
+const saltRounds = 10;
+
 export const insertUsuario = async (usuario: Usuario) => {
     const db = await dbConn();
-    const temp = new Usuario(usuario.email, usuario.senha);
+
+    const stringPass = usuario.senha;
+    const salt = await genSalt(saltRounds);
+    const encrypted = await hash(stringPass, salt);
+
+    const temp = new Usuario(usuario.email, encrypted);
     const obj = await db.insertOne(temp);
+
     return obj.ops;
 }
 
@@ -17,14 +25,20 @@ const getUsuarioByEmail = async (email: string) => {
     return await db.findOne({ "email": email });
 };
 
-//TODO implementar encriptação da senha antes de fazer a consulta.
 //TODO A consulta foi feita, mas não sei como lidar com o retorno que vem em outra mensagem...
 export const login = async (usuario: Usuario) => {
     const db = await dbConn();
-    const user = await db.findOne({ "email": usuario.email, "senha:": usuario.senha });
+
+    const user = await db.findOne({ "email": usuario.email });
     if (user) {
-        await prepareMessage(usuario.email, 'CHECK_LOGIN');
+
+        const checkPass = await compare(usuario.senha, user.senha);
+
+        if (checkPass) {
+            await prepareMessage(usuario.email, 'CHECK_LOGIN');
+        }
     }
+
     const ativo = true;
     if (ativo) {
         return user;
@@ -33,12 +47,18 @@ export const login = async (usuario: Usuario) => {
     }
 };
 
-//TODO implementar encriptação da senha antes de fazer a consulta.
 export const updatePasswordByEmail = async (usuario: UsuarioUpdate) => {
     const db = await dbConn();
+
     const temp = await getUsuarioByEmail(usuario.email);
-    if (temp.senha === usuario.senhaAtual) {
-        const userUpdated = new Usuario(usuario.email, usuario.senhaNova);
+    const checkActual = await compare(usuario.senhaAtual, temp.senha);
+
+    if (checkActual) {
+        const stringPass = usuario.senhaNova;
+        const salt = await genSalt(saltRounds);
+        const encrypted = await hash(stringPass, salt);
+
+        const userUpdated = new Usuario(usuario.email, encrypted);
         const obj = await db.replaceOne({ "_id": new ObjectId(temp.id) }, userUpdated);
 
         if (obj.modifiedCount) {
@@ -46,16 +66,16 @@ export const updatePasswordByEmail = async (usuario: UsuarioUpdate) => {
         } else {
             return false;
         }
-
     } else {
         return false;
     }
-
 };
 
 export const deleteUsuarioByEmail = async (email: string) => {
     const db = await dbConn();
+
     const temp = await getUsuarioByEmail(email);
     const obj = await db.deleteOne({ "_id": new ObjectId(temp.id) });
+
     return obj.deletedCount;
 };
